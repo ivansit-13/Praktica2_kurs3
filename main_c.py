b4a12bc5e3b22c0f0c3fdc7a0a6b125f33012803
Иван Ситников <gr632_siipe@lab-308-3.tomtit-tomsk.ru>
2026-04-21 18:43:36 +0700

    updateM

--- main_c.py
+++ main_c.py
@@ -1,5 +1,7 @@
-def on_startup():
-    print("Добро пожаловать в канкулятор Ивана Петровича!")
+print("Добро пожаловать в канкулятор Ивана Петровича!")
+
+
+while True:
     print("Пожалуйста выберите действие написав цифру 0-3")
     print("0 - вычитание")
     print("1 - сложение")
@@ -7,17 +9,15 @@ def on_startup():
     print("3 - деление")
 
 
-
-on_startup()
-action = int(input("Вы выбираете = "))
-if action == 0:
-    pass
-elif action == 1:
-    pass
-elif action == 2:
-    pass
-elif action == 3:
-    pass
-else:
-    print("Вы ввели не валидные данные")
-    print("Дух машины растроен и покидает вас")
+    action = int(input("Вы выбираете = "))
+    if action == 0:
+        pass
+    elif action == 1:
+        pass
+    elif action == 2:
+        pass
+    elif action == 3:
+        pass
+    else:
+        print("Вы ввели не валидные данные")
+        print("Дух машины растроен и покидает вас")
