d93755fed930ae8417cb911f23f7d50da8d6505d
Иван Ситников <gr632_siipe@lab-308-3.tomtit-tomsk.ru>
2026-04-21 18:51:18 +0700

    PlusCommit

--- main_c.py
+++ main_c.py
@@ -1,5 +1,11 @@
-def on_startup():
-    print("Добро пожаловать в канкулятор Ивана Петровича!")
+print("Добро пожаловать в канкулятор Ивана Петровича!")
+
+def plus_func():
+    a = int(input("Введите первое число "))
+    b = int(input("Введите второе число "))
+    print(f"{a} + {b} = {a+b}")
+
+while True:
     print("Пожалуйста выберите действие написав цифру 0-3")
     print("0 - вычитание")
     print("1 - сложение")
@@ -7,17 +13,15 @@ def on_startup():
     print("3 - деление")
 
 
-
-on_startup()
-action = int(input("Вы выбираете = "))
-if action == 0:
-    pass
-elif action == 1:
-    pass
-elif action == 2:
-    pass
-elif action == 3:
-    pass
-else:
-    print("Вы ввели не валидные данные")
-    print("Дух машины растроен и покидает вас")
+    action = int(input("Вы выбираете = "))
+    if action == 0:
+        pass
+    elif action == 1:
+        plus_func()
+    elif action == 2:
+        pass
+    elif action == 3:
+        pass
+    else:
+        print("Вы ввели не валидные данные")
+        print("Дух машины растроен и покидает вас")
