c5e958d569a6672667b9cff986db7b75a1b19d77
Иван Ситников <gr632_siipe@lab-308-3.tomtit-tomsk.ru>
2026-04-21 18:07:49 +0700

    Основа

---
+++ main_c.py
@@ -0,0 +1,23 @@
+def on_startup():
+    print("Добро пожаловать в канкулятор Ивана Петровича!")
+    print("Пожалуйста выберите действие написав цифру 0-3")
+    print("0 - вычитание")
+    print("1 - сложение")
+    print("2 - умножение")
+    print("3 - деление")
+
+
+
+on_startup()
+action = int(input("Вы выбираете = "))
+if action == 0:
+    pass
+elif action == 1:
+    pass
+elif action == 2:
+    pass
+elif action == 3:
+    pass
+else:
+    print("Вы ввели не валидные данные")
+    print("Дух машины растроен и покидает вас")
