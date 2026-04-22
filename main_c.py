1cf4a8ef9bc2c8856898ad4f92640e8ddd959ade
Иван Ситников <gr632_siipe@lab-308-6.tomtit-tomsk.ru>
2026-04-22 14:49:47 +0700

    minusCommit

--- main_c.py
+++ main_c.py
@@ -1,5 +1,11 @@
-def on_startup():
-    print("Добро пожаловать в канкулятор Ивана Петровича!")
+print("Добро пожаловать в канкулятор Ивана Петровича!")
+
+def fun_minus():
+    a = int(input("Введите 1 число, из которого будет вычитаться "))
+    b = int(input("Введите второе число, что будет вычитать "))
+    print(f"{a} - {b} = {a-b}")
+
+while True:
     print("Пожалуйста выберите действие написав цифру 0-3")
     print("0 - вычитание")
     print("1 - сложение")
@@ -7,17 +13,15 @@ def on_startup():
     print("3 - деление")
 
 
-
-on_startup()
-action = int(input("Вы выбираете = "))
-if action == 0:
-    pass
-elif action == 1:
-    pass
-elif action == 2:
-    pass
-elif action == 3:
-    pass
-else:
-    print("Вы ввели не валидные данные")
-    print("Дух машины растроен и покидает вас")
+    action = int(input("Вы выбираете = "))
+    if action == 0:
+        fun_minus()
+    elif action == 1:
+        pass
+    elif action == 2:
+        pass
+    elif action == 3:
+        pass
+    else:
+        print("Вы ввели не валидные данные")
+        print("Дух машины растроен и покидает вас")
